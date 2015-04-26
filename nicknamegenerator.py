771fdcea1b1174a99df09122eeef5984a9a047cf
mcvdenk <mvdenk@gmail.com>
2015-04-27 01:27:05 +0200

    added nicknames and changed condition to that line should be alphabetic

--- nicknamegenerator.py
+++ nicknamegenerator.py
@@ -3,6 +3,6 @@ import random
 namesfile = open('nicknames.txt','r')
 names = []
 for name in namesfile:
-	if (name != ""): names.append(name)
+	if (name.isalpha()): names.append(name).replace("\n","")
 selected = names[random.randrange(len(names))].replace("\n","")
 print(selected)
