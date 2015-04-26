f5520be2bf506648201cd472670773fd3f56ae0e
mcvdenk <mvdenk@gmail.com>
2015-04-27 01:08:25 +0200

    moved the not printing empty lines to adding to the list with names

--- nicknamegenerator.py
+++ nicknamegenerator.py
@@ -3,6 +3,6 @@ import random
 namesfile = open('nicknames.txt','r')
 names = []
 for name in namesfile:
-	names.append(name)
+	if (name != ""): names.append(name)
 selected = names[random.randrange(len(names))].replace("\n","")
-if (selected != ""): print(selected)
+print(selected)
