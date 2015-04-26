d48ecf164c7b9ce9dfd3f1ac707879eb15f85943
mcvdenk <mvdenk@gmail.com>
2015-04-27 01:06:59 +0200

    generator does not print the newline anymore and does not print empty lines

--- nicknamegenerator.py
+++ nicknamegenerator.py
@@ -4,4 +4,5 @@ namesfile = open('nicknames.txt','r')
 names = []
 for name in namesfile:
 	names.append(name)
-print(names[random.randrange(len(names))])
+selected = names[random.randrange(len(names))].replace("\n","")
+if (selected != ""): print(selected)
